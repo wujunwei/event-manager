7ea0da9d8266f2061885aca0f83854e947d7a931
wujunwei <wjw3323@live.com>
2016-12-13 23:39:40 +0800

    add event class

---
+++ src/EventManager.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wjw33
+ * Date: 2016-12-13
+ * Time: 23:39
+ */
+
+namespace FirstW\EventManager;
+
+
+use Psr\EventManager\EventInterface;
+use Psr\EventManager\EventManagerInterface;
+
+class EventManager implements EventManagerInterface
+{
+
+    /**
+     * Attaches a listener to an event
+     *
+     * @param string $event the event to attach too
+     * @param callable $callback a callable function
+     * @param int $priority the priority at which the $callback executed
+     * @return bool true on success false on failure
+     */
+    public function attach($event, $callback, $priority = 0)
+    {
+        // TODO: Implement attach() method.
+    }
+
+    /**
+     * Detaches a listener from an event
+     *
+     * @param string $event the event to attach too
+     * @param callable $callback a callable function
+     * @return bool true on success false on failure
+     */
+    public function detach($event, $callback)
+    {
+        // TODO: Implement detach() method.
+    }
+
+    /**
+     * Clear all listeners for a given event
+     *
+     * @param  string $event
+     * @return void
+     */
+    public function clearListeners($event)
+    {
+        // TODO: Implement clearListeners() method.
+    }
+
+    /**
+     * Trigger an event
+     *
+     * Can accept an EventInterface or will create one if not passed
+     *
+     * @param  string|EventInterface $event
+     * @param  object|string $target
+     * @param  array|object $argv
+     * @return mixed
+     */
+    public function trigger($event, $target = null, $argv = array())
+    {
+        // TODO: Implement trigger() method.
+    }
+}
